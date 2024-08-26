4c39d1b5ba4d0ec4cd33c379302f2333862ba971
Ahmedki1l <85763359+Ahmedki1l@users.noreply.github.com>
2024-08-26 13:32:51 +0300

    dall e 2 enhancing

--- api/openai_api_requests.py
+++ api/openai_api_requests.py
@@ -100,7 +100,7 @@ def image_creator(prompt):
             prompt=prompt,
             size="1024x1024",
             quality="hd",
-            style="natural",
+            style="vivid",
             n=1,
         )
 
